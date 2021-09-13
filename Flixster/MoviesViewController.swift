ba9dc7e5522b2e6ce80aa26b71d56be94a0b7cca
Skellinghoff <jarodwellinghoff@gmail.com>
2021-09-13 16:40:41 -0500

    Xcode Test

--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -68,18 +68,9 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
 		let posterURL = URL(string: baseURL + posterPath)!
 		
 		cell.posterView.af.setImage(withURL: posterURL)
-		
+		// Xcode Test
 		return cell
 	}
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	
 
 }
